bb624d37aa6f68e1f049bd9af9274d2c23b5b708
Roman Boiko <roman.boiko@gmail.com>
2022-04-28 17:50:48 +0200

    flights

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,6 @@ module.exports = {
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': 'error'
+    //'prettier/prettier': 'error'
   }
 }
